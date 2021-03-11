072cefa9edb354c75adbc96aea523a30815c7f83
venkata anil <anilvenkata@redhat.com>
2021-03-11 13:01:35 +0530

    security group logging testing

---
+++ log_sg/overcloud_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+openstack overcloud deploy \
+--timeout 240 \
+--templates /usr/share/openstack-tripleo-heat-templates \
+--stack overcloud \
+--libvirt-type kvm \
+--ntp-server clock1.rdu2.redhat.com \
+-e /home/stack/virt/config_lvm.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/virt/network/network-environment.yaml \
+-e /home/stack/virt/inject-trust-anchor.yaml \
+-e /home/stack/virt/hostnames.yml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-ha.yaml \
+-e /home/stack/virt/debug.yaml \
+-e /home/stack/virt/nodes_data.yaml \
+-e ~/containers-prepare-parameter.yaml \
+-e /home/stack/virt/docker-images.yaml \
+-e /home/stack/virt/api-policies.yaml \
+-e /home/stack/virt/log_sec_groups.yaml \
+-e /home/stack/virt/networking_ovn_hotfix_template.yaml \
+--log-file overcloud_deployment_53.log
