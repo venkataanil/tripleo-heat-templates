327d20bb18839aa21741a0ed65d967d75e83b91a
venkata anil <anilvenkata@redhat.com>
2021-04-20 10:52:12 +0000

    octavia templates

---
+++ octavia/overcloud_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+openstack overcloud deploy \
+--timeout 560 \
+--templates /usr/share/openstack-tripleo-heat-templates \
+  --environment-file /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
+--stack overcloud \
+--libvirt-type kvm \
+--ntp-server clock1.rdu2.redhat.com \
+-e /home/stack/virt/config_lvm.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/virt/network/network-environment.yaml \
+-e /home/stack/virt/inject-trust-anchor.yaml \
+-e /home/stack/virt/hostnames.yml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-ha.yaml \
+-e /home/stack/virt/debug.yaml \
+-e /home/stack/virt/nodes_data.yaml \
+-e ~/containers-prepare-parameter.yaml \
+-e /home/stack/virt/ovn_patch.yaml \
+-r /home/stack/roles_data.yaml \
+--log-file overcloud_deployment_80.log
